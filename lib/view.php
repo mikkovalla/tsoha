dfeb2e66c24bffccc059b4a3e62f55e47d607acc
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-11-25 04:49:13 +0200

    view session tarkistus muutettu

--- lib/view.php
+++ lib/view.php
@@ -1,51 +1,57 @@
 <?php
 
-  class View{
-
-    public static function make($view, $content = array()){
-      // Alustetaan Twig
+  class View
+  {
+      public static function make($view, $content = array())
+      {
+          // Alustetaan Twig
       $twig = self::get_twig();
 
-      try{
-        // Asetetaan uudelleenohjauksen yhteydessä lisätty viesti
+          try {
+              // Asetetaan uudelleenohjauksen yhteydessä lisätty viesti
         self::set_flash_message($content);
 
         // Asetetaan näkymään base_path-muuttuja index.php:ssa määritellyllä BASE_PATH vakiolla
         $content['base_path'] = BASE_PATH;
 
-        // Asetetaan näkymään kirjautunut käyttäjä, jos get_user_logged_in-metodi on toteutettu
-        if(method_exists('BaseController', 'get_user_logged_in')){
-          $content['user_logged_in'] = BaseController::get_user_logged_in();
+        // Asetetaan näkymään kirjautunut employee, jos get_employee_logged_in-metodi on toteutettu
+        if (method_exists('BaseController', 'get_employee_logged_in')) {
+            $content['employee_logged_in'] = BaseController::get_employee_logged_in();
+        }
+
+        // Asetetaan näkymään kirjautunut employer, jos get_employer_logged_in-metodi on toteutettu
+        if (method_exists('BaseController', 'get_employer_logged_in')) {
+            $content['employer_logged_in'] = BaseController::get_employer_logged_in();
         }
 
         // Tulostetaan Twig:n renderöimä näkymä
         echo $twig->render($view, $content);
-      } catch (Exception $e){
-        die('Virhe näkymän näyttämisessä: ' . $e->getMessage());
+          } catch (Exception $e) {
+              die('Virhe näkymän näyttämisessä: '.$e->getMessage());
+          }
+
+          exit();
       }
 
-      exit();
-    }
+      private static function get_twig()
+      {
+          Twig_Autoloader::register();
 
-    private static function get_twig(){
-      Twig_Autoloader::register();
+          $twig_loader = new Twig_Loader_Filesystem('app/views');
 
-      $twig_loader = new Twig_Loader_Filesystem('app/views');
-
-      return new Twig_Environment($twig_loader);
-    }
-
-    private static function set_flash_message(&$content){
-      if(isset($_SESSION['flash_message'])){
-
-        $flash = json_decode($_SESSION['flash_message']);
-
-        foreach($flash as $key => $value){
-          $content[$key] = $value;
-        }
-
-        unset($_SESSION['flash_message']);
+          return new Twig_Environment($twig_loader);
       }
-    }
 
+      private static function set_flash_message(&$content)
+      {
+          if (isset($_SESSION['flash_message'])) {
+              $flash = json_decode($_SESSION['flash_message']);
+
+              foreach ($flash as $key => $value) {
+                  $content[$key] = $value;
+              }
+
+              unset($_SESSION['flash_message']);
+          }
+      }
   }
