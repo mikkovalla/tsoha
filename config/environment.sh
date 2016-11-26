4f317936c7446ff704719d8412a0905d194279a1
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-11-26 12:36:58 +0200

    sync

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="mikvalla"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="tsoha_projekti"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
