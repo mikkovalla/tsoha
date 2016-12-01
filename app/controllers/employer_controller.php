57988e8310fab8973496b14c4badd707488f5be6
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2016-12-01 06:00:04 +0200

    controllerit lisätty ja päivitetty

---
+++ app/controllers/employer_controller.php
@@ -0,0 +1,50 @@
+<?php
+
+require 'app/models/employer.php';
+/**
+ *
+ */
+class EmployerController extends BaseController
+{
+    public static function login()
+    {
+        View::make('employer/login_employer.html');
+    }
+    public static function handle_login()
+    {
+        $params = $_POST;
+
+        $employer = Employer::auth($params['username'], $params['password']);
+
+        if (!$employer) {
+            View::make('employer/login_employer.html', array('error' => 'Väärä käyttäjätunnus tai salasana!', 'username' => $params['username']));
+        } else {
+            #Kint::dump($employer);
+            $_SESSION['employer'] = $employer->id;
+            Redirect::to('/employer/employer.html', array('message' => 'Tervetuloa taas '.$employer->name.'!'));
+        }
+    }
+
+    public static function register()
+    {
+        View::make('employer/register_employer.html');
+    }
+
+    public static function handle_register()
+    {
+        $params = $_POST;
+        if (!Employer::checkUsernameEmployer($params['username'])) {
+            $employer = new Employer(array(
+          'company_name' => $params['company_name'],
+          'email' => $params['email'],
+          'username' => $params['username'],
+          'password' => $params['password'],
+          'company_description' => $params['company_description'],
+          'created' => date('dd/mm/YY'),
+        ));
+            $employer->newEmployer();
+
+            Redirect::to('employer/login_employer.html', array('message' => 'Voit nyt kirjautua sisään '.$employer->name.'!'));
+        }
+    }
+}
--- app/controllers/jobs_controller.php
+++ app/controllers/jobs_controller.php
@@ -1,17 +1,20 @@
 <?php
 
 require 'app/models/jobs.php';
-
+require 'app/models/employer.php';
+require 'app/models/types.php';
 
 class JobsController extends BaseController
 {
     public static function index()
     {
+        $employers = Employer::allEmployers();
+        $types = Type::allTypes();
         $jobs = Jobs::all();
-        View::make('home.html', array('jobs' => $jobs));
+        View::make('home.html', array('jobs' => $jobs, 'employers' => $employers, 'types' => $types));
     }
 
-    public static function addjob()
+    public static function addjob($employer_id)
     {
         #todo job per employer id
         $params = $_POST;
@@ -24,7 +27,9 @@ class JobsController extends BaseController
 
     public static function details($id)
     {
-      $job = Jobs::find($id);
-      View::make('jobs/details.html', array('jobs' => $job));
+        $job = Jobs::find($id);
+        $types = Type::allTypes();
+        $employers = Employer::allEmployers();
+        View::make('jobs/details.html', array('jobs' => $job, 'employers' => $employers, 'types' => $types));
     }
 }
